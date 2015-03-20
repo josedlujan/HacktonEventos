fac3af92de345c815faf7483853c66787d38212e
Omar Guerrero <guemuno@gmail.com>
2015-03-20 11:20:15 -0500

    Commit inicial

---
+++ EventosCancun/EventosCancun/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventosCancun.Startup))]
+namespace EventosCancun
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
