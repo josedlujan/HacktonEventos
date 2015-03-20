033353af1d4c10a90a41401a8537b205d8e884bc
MarcoSanchez <marcos.sanchez@devfacto.com>
2015-03-20 13:25:12 -0400

    poner clases de controler privada

--- EventosCancun/EventosCancun/Controllers/HomeController.cs
+++ EventosCancun/EventosCancun/Controllers/HomeController.cs
@@ -6,20 +6,21 @@ using System.Web.Mvc;
 
 namespace EventosCancun.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        [ValidateAntiForgeryToken]
         public ActionResult Index()
         {
             return View();
         }
-
+        [ValidateAntiForgeryToken]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
